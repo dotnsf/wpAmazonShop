bdb94c65c2452ff58e94d5d2f9cdf868a51fa4cb
root <root@rylics.dot123.net>
2017-07-17 19:18:27 +0900

    #2 fixed.

--- main.php
+++ main.php
@@ -7,13 +7,17 @@ $nodes = array(
 //	"52908051",  //. ビューティー/ヘアケア
 //	"52907051",  //. ビューティー/ボディケア
 //	"52912051"   //. ビューティー/男性化粧品
-//	"169772011"  //. ビューティー/オーラルケア/ホワイトニング
-	"5263226051" //. ビューティー/スキンケア・ボディケア/アイケア/アイクリーム
+	"169772011"  //. ビューティー/オーラルケア/ホワイトニング
+//	"5263226051" //. ビューティー/スキンケア・ボディケア/アイケア/アイクリーム
 );
+
+//$records = json_decode( file_get_contents( "./records.json" ), true );
 
 foreach( $nodes as $node ){
 	getCodesFromAmazonAPI( $node );
 }
+
+//file_put_contents( "./records.json", json_encode($records) );
 
 //. $records の中身をデータベースへ
 echo( "#records = " . count($records) . "¥n" );
--- pdo.php
+++ pdo.php
@@ -82,27 +82,29 @@ function importToWPDB( $recordArray ){
 			//. Insert
 			try{
 				//.  この商品は登録済みかどうかのチェックをする
-				$sql0 = "select count(*) as cnt from " . TABLE_PREFIX . "posts where post_title = '" . $title . "'";
-				$stmt0 = $pdo->query( $sql0 );
-				if( $row = $stmt0->fetch( PDO::FETCH_ASSOC ) ){
-					$cnt = ( int )$row['cnt'];
-					if( $cnt == 0 ){
-						$sql1 = "insert into " . TABLE_PREFIX . "posts(post_title, post_content, post_date ) values( :title, :content, :post_date )";
-						$stmt1 = $pdo->prepare($sql1);
-						$stmt1->bindParam(':title', $title, PDO::PARAM_STR);
-						$stmt1->bindParam(':content', $content, PDO::PARAM_STR);
-						$stmt1->bindParam(':post_date', date("Y-m-d H:i:s"), PDO::PARAM_STR);
-						$r = $stmt1->execute();
-						echo( "(1)($i) : " . $title . " -> " . $r . "\n" );
+				$sql0 = "select count(*) as cnt from " . TABLE_PREFIX . "posts where post_title = ?";
+				$stmt0 = $pdo->prepare( $sql0 );
+				if( $stmt0->execute( array( $title ) )){
+					if( $row = $stmt0->fetch() ){
+						$cnt = ( int )$row['cnt'];
+						if( $cnt == 0 ){
+							$sql1 = "insert into " . TABLE_PREFIX . "posts(post_title, post_content, post_date ) values( :title, :content, :post_date )";
+							$stmt1 = $pdo->prepare($sql1);
+							$stmt1->bindParam(':title', $title, PDO::PARAM_STR);
+							$stmt1->bindParam(':content', $content, PDO::PARAM_STR);
+							$stmt1->bindParam(':post_date', date("Y-m-d H:i:s"), PDO::PARAM_STR);
+							$r = $stmt1->execute();
+							echo( "(1)($i) : " . $title . " -> " . $r . "\n" );
 
-						//. 直前の insert で挿入された ID を取得
-						if( $r ){
-							$sql2 = "select last_insert_id() as id from " . TABLE_PREFIX . "posts";
-							$stmt2 = $pdo->query( $sql2 );
-							if( $row = $stmt2->fetch( PDO::FETCH_ASSOC ) ){
-								$ID = ( int )$row['id'];
-								echo( "(1)($i) :  -> " . $ID . "\n" );
-								$recordArray[$i]['ID'] = $ID;
+							//. 直前の insert で挿入された ID を取得
+							if( $r ){
+								$sql2 = "select last_insert_id() as id from " . TABLE_PREFIX . "posts";
+								$stmt2 = $pdo->query( $sql2 );
+								if( $row = $stmt2->fetch( PDO::FETCH_ASSOC ) ){
+									$ID = ( int )$row['id'];
+									echo( "(1)($i) :  -> " . $ID . "\n" );
+									$recordArray[$i]['ID'] = $ID;
+								}
 							}
 						}
 					}
@@ -122,55 +124,63 @@ function importToWPDB( $recordArray ){
 		//. (3) (2) の結果を wp_terms テーブルの name と slug にインサートして、同時にその際に生成された term_id を記録
 		$terms = array();
 		while( list( $category, $num ) = each( $categorys ) ){
+			echo( "(3)category = " . $category );
+
 			//. Insert
 			try{
-				//. (未対応) カテゴリが既にDBに存在している可能性も考慮する
-				$sql0 = "select count(*) as cnt from " . TABLE_PREFIX . "terms where name = '" . $category . "'";
-				$stmt0 = $pdo->query( $sql0 );
-				if( $row = $stmt0->fetch( PDO::FETCH_ASSOC ) ){
-					$cnt = ( int )$row['cnt'];
-					if( $cnt == 0 ){
-						$sql1 = "insert into " . TABLE_PREFIX . "terms( name, slug ) values( :name, :slug )";
-						$stmt1 = $pdo->prepare($sql1);
-						$stmt1->bindParam(':name', $category, PDO::PARAM_STR);
-						$stmt1->bindParam(':slug', urlencode($category), PDO::PARAM_STR);
-						$r = $stmt1->execute();
-						echo( "(3)(" . $category . "): -> " . $r . "\n" );
+				//. カテゴリが既にDBに存在している可能性も考慮する
+				$sql0 = "select count(*) as cnt from " . TABLE_PREFIX . "terms where name = ?";
+				$stmt0 = $pdo->prepare( $sql0 );
+				if( $stmt0->execute( array( $category ) ) ){
+					if( $row = $stmt0->fetch() ){
+						$cnt = ( int )$row['cnt'];
+						if( $cnt == 0 ){
+							$sql1 = "insert into " . TABLE_PREFIX . "terms( name, slug ) values( :name, :slug )";
+							$stmt1 = $pdo->prepare($sql1);
+							$stmt1->bindParam(':name', $category, PDO::PARAM_STR);
+							$stmt1->bindParam(':slug', urlencode($category), PDO::PARAM_STR);
+							$r = $stmt1->execute();
+							echo( " (3)(" . $category . "): -> inserted. " . $r . "\n" );
 
-						//. 直前の insert で挿入された ID を取得
-						if( $r ){
-							$sql2 = "select last_insert_id() as term_id from " . TABLE_PREFIX . "terms";
-							$stmt2 = $pdo->query( $sql2 );
-							if( $row = $stmt2->fetch( PDO::FETCH_ASSOC ) ){
-								$term_id = ( int )$row['term_id'];
-								echo( "(3)(" . $category . ") :  -> " . $term_id . "\n" );
-		
-								//. (4) (3) の結果を wp_term_taxonomy テーブルの term_id にインサートして、同時にその際に生成された term_taxonomy_id を記録
-								$sql3 = "insert into " . TABLE_PREFIX . "term_taxonomy( term_id, taxonomy, count ) values( :term_id, 'category', :count )";
-								$stmt3 = $pdo->prepare($sql3);
-								$stmt3->bindParam(':term_id', $term_id, PDO::PARAM_STR);
-								$stmt3->bindParam(':count', $num , PDO::PARAM_INT);
-								$r = $stmt3->execute();
-								echo( "(4)(" . $category . ") :  -> " . $r . "\n" );
+							//. 直前の insert で挿入された ID を取得
+							if( $r ){
+								$sql2 = "select last_insert_id() as term_id from " . TABLE_PREFIX . "terms";
+								$stmt2 = $pdo->query( $sql2 );
+								if( $row = $stmt2->fetch( PDO::FETCH_ASSOC ) ){
+									$term_id = ( int )$row['term_id'];
+									echo( "  (3)(" . $category . ") :  -> term_id = " . $term_id . "\n" );
+			
+									//. (4) (3) の結果を wp_term_taxonomy テーブルの term_id にインサートして、同時にその際に生成された term_taxonomy_id を記録
+									$sql3 = "insert into " . TABLE_PREFIX . "term_taxonomy( term_id, taxonomy, count ) values( :term_id, 'category', :count )";
+									$stmt3 = $pdo->prepare($sql3);
+									$stmt3->bindParam(':term_id', $term_id, PDO::PARAM_STR);
+									$stmt3->bindParam(':count', $num , PDO::PARAM_INT);
+									$r = $stmt3->execute();
+									echo( "   (4)(" . $category . ") :  -> " . $r . "\n" );
 
-								//. 直前の insert で挿入された ID を取得
-								if( $r ){
-									$sql4 = "select last_insert_id() as term_taxonomy_id from " . TABLE_PREFIX . "term_taxonomy";
-									$stmt4 = $pdo->query( $sql4 );
-									if( $row = $stmt4->fetch( PDO::FETCH_ASSOC ) ){
-										$term_taxonomy_id = ( int )$row['term_taxonomy_id'];
-										$terms[$category] = $term_taxonomy_id;
+									//. 直前の insert で挿入された ID を取得
+									if( $r ){
+										$sql4 = "select last_insert_id() as term_taxonomy_id from " . TABLE_PREFIX . "term_taxonomy";
+										$stmt4 = $pdo->query( $sql4 );
+										if( $row = $stmt4->fetch( PDO::FETCH_ASSOC ) ){
+											$term_taxonomy_id = ( int )$row['term_taxonomy_id'];
+											$terms[$category] = $term_taxonomy_id;
+											echo( "    (term_taxonomy_id) :  -> " . $term_taxonomy_id . "\n" );
+										}
 									}
 								}
 							}
-						}
-					}else{
-						//. 対応する既存の term_taxonomy_id を見つける必要がある
-						$sql1 = "select " . TABLE_PREFIX . ".term_taxonomy_id as term_taxonomy_id from " . TABLE_PREFIX . "taxonomy, " . TABLE_PREFIX . "terms where " . TABLE_PREFIX . "terms.name = '" . $category . "' and " . TABLE_PREFIX . "terms.term_id = " . TABLE_PREFIX . "taxonomy.term_id";
-						$stmt1 = $pdo->query( $sql1 );
-						if( $row = $stmt1->fetch( PDO::FETCH_ASSOC ) ){
-							$term_taxonomy_id = ( int )$row['term_taxonomy_id'];
-							$terms[$category] = $term_taxonomy_id;
+						}else{
+							//. 対応する既存の term_taxonomy_id を見つける必要がある
+							$sql1 = "select " . TABLE_PREFIX . ".term_taxonomy_id as term_taxonomy_id from " . TABLE_PREFIX . "taxonomy, " . TABLE_PREFIX . "terms where " . TABLE_PREFIX . "terms.name = ? and " . TABLE_PREFIX . "terms.term_id = " . TABLE_PREFIX . "taxonomy.term_id";
+							$stmt1 = $pdo->prepare( $sql1 );
+							if( $stmt1->execute( array( $category ) ) ){
+								if( $row = $stmt1->fetch( PDO::FETCH_ASSOC ) ){
+									$term_taxonomy_id = ( int )$row['term_taxonomy_id'];
+									$terms[$category] = $term_taxonomy_id;
+									echo( " (3)(" . $category . "): -> found. term_taxonomy_id = " . $term_taxonomy_id . "\n" );
+								}
+							}
 						}
 					}
 				}
